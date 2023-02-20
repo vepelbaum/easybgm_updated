ae045308df9d2ee5c0755f4996ca17eb37aafa19
KarolineHuth <karoline.huth@gmail.com>
2023-02-20 09:46:28 +0100

    update edge prior BF

--- R/bgm_extract.R
+++ R/bgm_extract.R
@@ -23,7 +23,7 @@ bgm_extract <- function(fit, method, edge.prior = 0.5, package = "BDgraph", post
       diag(bdgraph_res$estimates_bma) <- 0
       bdgraph_res$inc_probs <- as.matrix(BDgraph::plinks(fit))
       bdgraph_res$inc_probs  <- bdgraph_res$inc_probs + t(bdgraph_res$inc_probs)
-      bdgraph_res$BF <- (bdgraph_res$inc_probs / (1 - bdgraph_res$inc_probs))/(prior.edge/(1-prior.edge))
+      bdgraph_res$BF <- (bdgraph_res$inc_probs / (1 - bdgraph_res$inc_probs))/(edge.prior /(1-edge.prior))
       bdgraph_res$structure_bma <- 1*(bdgraph_res$inc_probs > 0.5)
       bdgraph_res$structure_probabilities <- fit$graph_weights/sum(fit$graph_weights)
       bdgraph_res$graph_weights <- fit$graph_weights
@@ -54,7 +54,7 @@ bgm_extract <- function(fit, method, edge.prior = 0.5, package = "BDgraph", post
       diag(bdgraph_res$estimates_bma) <- 0
       bdgraph_res$inc_probs <- as.matrix(BDgraph::plinks(fit))
       bdgraph_res$inc_probs  <- bdgraph_res$inc_probs + t(bdgraph_res$inc_probs)
-      bdgraph_res$BF <- (bdgraph_res$inc_probs / (1 - bdgraph_res$inc_probs))/(prior.edge/(1-prior.edge))
+      bdgraph_res$BF <- (bdgraph_res$inc_probs / (1 - bdgraph_res$inc_probs))/(edge.prior/(1-edge.prior))
       bdgraph_res$structure_bma <- 1*(bdgraph_res$inc_probs > 0.5)
       bdgraph_res$structure_probabilities <- fit$graph_weights/sum(fit$graph_weights)
       bdgraph_res$graph_weights <- fit$graph_weights
@@ -84,7 +84,7 @@ bgm_extract <- function(fit, method, edge.prior = 0.5, package = "BDgraph", post
       bdgraph_res <- list()
       #Bayesian model-averaged estimates
       bdgraph_res$inc_probs <- as.matrix(BDgraph::plinks(fit))
-      bdgraph_res$BF <- (bdgraph_res$inc_probs / (1 - bdgraph_res$inc_probs))/(prior.edge/(1-prior.edge))
+      bdgraph_res$BF <- (bdgraph_res$inc_probs / (1 - bdgraph_res$inc_probs))/(edge.prior/(1-edge.prior))
       bdgraph_res$package <- "BDgraph"
       bdgraph_res$model <- "dgm-binary"
 
