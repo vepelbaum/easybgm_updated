fd0c5fce5ba2d1e56a763fcb0445627e3419c79a
KarolineHuth <karoline.huth@gmail.com>
2023-05-10 17:40:21 +0200

    bug fixes

--- R/PlottingFunctions.R
+++ R/PlottingFunctions.R
@@ -30,7 +30,6 @@ plot_posteriorstructure <- function(output, as.BF = FALSE) {
       theme_classic()+
       labs(x = "Structures",
            y = expression(log(BF[1][s])))+
-      geom_hline(yintercept = 1/10, linetype = "dashed", size = 1.5)  +
       theme(panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(), axis.line = element_line(colour = "black", linewidth = 1.1),
             axis.text = element_text(size = 14), axis.title = element_text(size = 16),
@@ -89,7 +88,6 @@ plot_posteriorcomplexity <- function(output) {
           axis.ticks = element_line(size= .8), legend.text = element_text(size=14),
           axis.title.x = element_text(size=18,face="bold"),
           axis.title.y = element_text(size=18,face="bold"),
-          text=element_text(  family="Times New Roman"),
           panel.grid.major = element_blank()
     )
 }
@@ -155,8 +153,7 @@ plot_edgeevidence <- function(output, evidence_thresh = 10, split = F, show = c(
       )
     }
     if(length(show) != 3){
-      graph_show <- graph
-      graph_show <- 0
+      graph_show <- matrix(0, ncol = ncol(graph), nrow = nrow(graph))
       if("included" %in% show){
         graph_show[output$BF > evidence_thresh] <- 1
       }
@@ -164,7 +161,7 @@ plot_edgeevidence <- function(output, evidence_thresh = 10, split = F, show = c(
         graph_show[output$BF < (1/evidence_thresh)] <- 1
       }
       if("inconclusive" %in% show){
-        graph_show[output$BF > (1/evidence_thresh) & output$BF < evidence_thresh] <- 1
+        graph_show[(output$BF > (1/evidence_thresh)) & (output$BF < evidence_thresh)] <- 1
       }
       diag(graph_show) <- 1
       colnames(graph_show) <- colnames(output$parameters)
